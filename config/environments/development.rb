3a2c4a4b90ece5c0d789c047f7bbdaf1abbf624f
Tom1 <dongtangjin1@gmail.com>
2015-01-09 16:04:06 +0800

    rails g controller welcome index add controller filter add email support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,15 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "gmail.com",
+    authentication: :login,
+    user_name:      "coul201306",
+    password:       "goldbridge123",
+    enable_starttls_auto: true
+  }
 end
